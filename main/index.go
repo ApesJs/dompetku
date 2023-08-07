5311577c7ea387c45e107611ff9f852da1ff9222
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2023-08-07 16:27:15 +0700

    update

--- main/index.go
+++ main/index.go
@@ -14,23 +14,34 @@ func main() {
 	}
 	defer db.Close()
 
-	//LOGIN
-	var username, password string
-	fmt.Print("Username: ")
-	fmt.Scan(&username)
-	fmt.Print("Password: ")
-	fmt.Scan(&password)
+	//MENU
+	var menu int
+	fmt.Println("1. Login")
+	fmt.Println("2. Register")
+	fmt.Print("Pilih Menu : ")
+	fmt.Scan(&menu)
 
-	query := "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?"
-	var count int
-	err = db.QueryRow(query, username, password).Scan(&count)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if menu == 1 {
+		//LOGIN
+		var username, password string
+		fmt.Print("Username: ")
+		fmt.Scan(&username)
+		fmt.Print("Password: ")
+		fmt.Scan(&password)
 
-	if count > 0 {
-		fmt.Println("Login berhasil!")
+		query := "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?"
+		var count int
+		err = db.QueryRow(query, username, password).Scan(&count)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if count > 0 {
+			fmt.Println("Login berhasil!")
+		} else {
+			fmt.Println("Login gagal.")
+		}
 	} else {
-		fmt.Println("Login gagal.")
+		//REGISTER
 	}
 }
