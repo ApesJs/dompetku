b716cbd0de11ac2db2bef1011f74b67c1149c2e9
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2023-08-08 21:17:26 +0700

    update

---
+++ main/main_menu/main_menu.go
@@ -0,0 +1,15 @@
+package main_menu
+
+import "fmt"
+
+func MainMenu() {
+	fmt.Println("")
+	fmt.Println("1. My Profile")
+	fmt.Println("2. Search Profile")
+	fmt.Println("3. Transaction")
+	fmt.Println("4. History Transaction")
+	fmt.Println("5. Logout")
+	fmt.Println("")
+	fmt.Println("0. Exit")
+	fmt.Println("")
+}
--- main/profile/read_account.go
+++ main/profile/read_account.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"group-project/helper"
+	"group-project/main/main_menu"
 	"log"
 )
 
@@ -32,5 +33,32 @@ func ReadAccount(username string, db *sql.DB) {
 	fmt.Printf("%-15s: %s\n", "Address", address)
 	fmt.Printf("%-15s: %s\n", "Phone Number", phone_number)
 	fmt.Printf("%-15s: %s\n", "Date of Birth", date_of_birth)
+
+	var menu int
+	fmt.Println("")
+	fmt.Println("1 Edit My Profile")
+	fmt.Println("2 Delete My Profile")
+	fmt.Println("3 Back")
+	fmt.Println("")
+	fmt.Println("0 Exit")
+	fmt.Println("")
+	fmt.Print("Select Menu : ")
+	fmt.Scan(&menu)
+
+	helper.ClearConsole()
+
+	if menu == 1 {
+		fmt.Println("EDIT COMMING SOON")
+	} else if menu == 2 {
+		fmt.Println("DELETE COMMING SOON")
+	} else if menu == 3 {
+		main_menu.MainMenu()
+		fmt.Print("Select Menu : ")
+		fmt.Scan(&menu)
+
+		if menu == 1 {
+			ReadAccount(username, db)
+		}
+	}
 
 }
