a3e5c9ed78d20bace4cd958be993b572934062c9
Asep Jaenudin Sutarji <asepjaenudinsutarji@gmail.com>
2023-08-16 08:43:36 +0700

    update

--- main/main_menu/main_menu.go
+++ main/main_menu/main_menu.go
@@ -1,16 +1,6 @@
 package main_menu
 
 func MainMenu(channelMainMenu chan string) {
-	// fmt.Println("")
-	// fmt.Println("1. My Profile")
-	// fmt.Println("2. Search Profile")
-	// fmt.Println("3. Transaction")
-	// fmt.Println("4. History Transaction")
-	// fmt.Println("5. Logout")
-	// fmt.Println("")
-	// fmt.Println("0. Exit")
-	// fmt.Println("")
-
 	mainmenu := []string{
 		"1. My Profile",
 		"2. Search Profile",
